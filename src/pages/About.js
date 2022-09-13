d9e5ad6a163de0a556f05eb9464f0b45c1d03aa1
Bruno Santos <bruno.matasantos@gmail.com>
2022-09-13 14:47:22 -0300

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, distinctio sint dolorem molestiae at ullam! Repellat, deleniti veniam? Sint sapiente cum nemo modi enim fugit blanditiis corrupti aliquid minima. In minus quasi amet illo molestiae necessitatibus quas distinctio mollitia iure!
+      </p>
+    </section>
+  )
+}
+
+export default About
